0bdf8e1a07bcecdd58bddd166929e42d33338020
Becky Brown <bardbarienne@gmail.com>
2021-05-19 15:17:45 -0400

    no more avenir. bad font.

--- src/components/App.vue
+++ src/components/App.vue
@@ -44,10 +44,10 @@ export default {
 <style>
 body {
   margin: 0;
-  min-height: 100vh;
+  /* min-height: 100vh; */
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
