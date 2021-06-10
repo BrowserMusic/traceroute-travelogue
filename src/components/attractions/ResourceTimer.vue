0036e2c09a270a0f14adf6336e5ea75b55a16551
Becky Brown <bardbarienne@gmail.com>
2021-06-10 19:14:13 -0400

    fixing zak bugs

---
+++ src/components/attractions/ResourceTimer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="resource-timer"></div>
+</template>
+
+<script>
+export default {
+  computed: {
+    sceneIndex() {
+      return this.$store.state.path.sceneIndex;
+    },
+  },
+  watch: {
+    sceneIndex() {
+      // console.log("timer");
+      // this.getResources();
+    },
+  },
+  mounted() {
+    this.getResources();
+  },
+  methods: {
+    getResources() {
+      if (
+        !("performance" in window) ||
+        !("getEntriesByType" in window.performance) ||
+        !(window.performance.getEntriesByType("resource") instanceof Array)
+      ) {
+        document.getElementById("rt-unsupported").classList.remove("hidden");
+      } else {
+        // window.addEventListener("load", function () {
+        var resources = window.performance.getEntriesByType("resource");
+        console.log(resources);
+        // for(var obj in resources) {
+        //   console.log
+        //  var list = '';
+        //  for(var properties in resources[obj]) {
+        //     list += '<li>' + properties + ': <span class="value">' + resources[obj][properties] + '</span></li>';
+        //  }
+        //  document.getElementById(resources[obj].initiatorType + '-list').innerHTML = list;
+        // }
+        // });
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
